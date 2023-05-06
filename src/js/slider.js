b91f4bcab120d64ff2ed40bf8d7af2c2259b0955
Andrii Kurshatsov <andreswebit@ukr.net>
2023-05-06 21:44:04 +0300

    Add slider number

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,12 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   // direction: 'vertical',
-  SlidesPerView: 1,
+  // SlidesPerView: 1,
   spaceBetween: 10,
   loop: true,
-  keyboard: {
-    enabled: true,
-  },
+  // keyboard: {
+  //   enabled: true,
+  // },
   // If we need pagination
   pagination: {
     // el: '.swiper-pagination',
@@ -22,4 +22,21 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     // el: '.swiper-scrollbar',
   },
+  slidesPerView: 1,
+  speed: 1000,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  on: {
+    slideChange() {
+      let ollSliders = this.slides.length,
+        slIndex = this.realIndex + 1,
+        numIndex = (slIndex <= 9) ? '0' : '',
+        numOll = (ollSliders <= 9) ? '0' : '';
+      this.el.parentNode.querySelector(".carousel-number-of-watches").innerHTML = numIndex.toString() + slIndex.toString() + '<span class="dash"></span>' + numOll.toString() + ollSliders.toString();
+    }
+  }
+
 });
