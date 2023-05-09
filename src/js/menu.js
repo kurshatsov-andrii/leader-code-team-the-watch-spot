71555999a7d92f21ad063b4ba65911a408599c01
Andrii Kurshatsov <andreswebit@ukr.net>
2023-05-09 11:40:12 +0300

    Add new js for burger-menu and del border-color

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,13 +3,15 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    closeMenuLink: document.querySelectorAll('.burger-menu-link'),
   };
-
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
   }
+  refs.closeMenuLink.forEach(item =>
+    item.addEventListener('click', toggleMenu)
+  );
 })();
